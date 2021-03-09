da14bcb0560f3e47e9a19125dffd3b9f9e560df3
Matt Adam <matthewjamesadam@gmail.com>
2021-03-08 22:49:03 -0800

    Set correct initial async action state (#16)

--- client/useAsyncAction.ts
+++ client/useAsyncAction.ts
@@ -2,24 +2,24 @@ import * as React from 'react';
 import { DependencyList } from 'react';
 
 export function useAsyncAction<Result>(
-    action: () => Promise<Result>
+    action: () => Promise<Result>,
+    initialState: boolean = false
 ): [() => void, boolean, Result | null, Error | null] {
-    const [isRunning, setIsRunning] = React.useState(false);
+    const [isRunning, setIsRunning] = React.useState(initialState);
     const [result, setResult] = React.useState<Result | null>(null);
     const [error, setError] = React.useState<Error | null>(null);
 
-    const execute = () => {
+    const execute = async () => {
         setIsRunning(true);
-        action()
-            .then(res => {
-                setResult(res);
-            })
-            .catch(err => {
-                setError(err);
-            })
-            .finally(() => {
-                setIsRunning(false);
-            });
+
+        try {
+            const res = await action();
+            setResult(res);
+        } catch (error) {
+            setError(error);
+        } finally {
+            setIsRunning(false);
+        }
     };
 
     return [execute, isRunning, result, error];
@@ -29,16 +29,12 @@ export function useAsyncActionNow<Result>(
     action: () => Promise<Result>,
     deps?: DependencyList
 ): [boolean, Result | null, Error | null] {
-
-    const [execute, isRunning, result, error] = useAsyncAction(action);
+    const [execute, isRunning, result, error] = useAsyncAction(action, true);
     React.useEffect(execute, deps);
 
-    return [ isRunning, result, error] ;
+    return [isRunning, result, error];
 }
 
-export function useAsyncActionOnce<Result>(
-    action: () => Promise<Result>
-): [boolean, Result | null, Error | null] {
-
+export function useAsyncActionOnce<Result>(action: () => Promise<Result>): [boolean, Result | null, Error | null] {
     return useAsyncActionNow(action, []);
 }
