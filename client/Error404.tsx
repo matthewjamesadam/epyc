dcef83221009f173b946711e724487f37661c331
Matt <matthewjamesadam@gmail.com>
2021-03-11 10:23:05 -0800

    test

--- client/Error404.tsx
+++ client/Error404.tsx
@@ -3,7 +3,7 @@ import { EmojiPerson } from './EmojiPerson';
 
 export default function Error404(): JSX.Element {
     return (
-        <p className="mt-5 text-center display-1">
+        <p className="mt-4 text-center display-1">
             <EmojiPerson type="shrugger" />
         </p>
     );
