1419787414876457c32e9be1888348dbb5c15270
Matt <matthewjamesadam@gmail.com>
2021-03-16 13:11:21 -0700

    Pass slack signing secret in from AWS

--- server/aws_start.sh
+++ server/aws_start.sh
@@ -4,6 +4,7 @@ source /home/ec2-user/.bash_profile
 
 export DB_CONNECTION_STRING=$(aws ssm get-parameters --region ca-central-1 --output text --names epyc-DB_CONNECTION_STRING --with-decryption --query Parameters[0].Value)
 export DISCORD_BOT_TOKEN=$(aws ssm get-parameters --region ca-central-1 --output text --names epyc-DISCORD_BOT_TOKEN --with-decryption --query Parameters[0].Value)
+export SLACK_REQUEST_SIGNING_SECRET=$(aws ssm get-parameters --region ca-central-1 --output text --names epyc-SLACK_REQUEST_SIGNING_SECRET --with-decryption --query Parameters[0].Value)
 export PORT=3001
 
 cd /home/www/epyc
