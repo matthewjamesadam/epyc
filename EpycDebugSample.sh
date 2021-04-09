6ca52e21514169523c9263d6c959b2461c7def0a
Matt <matthewjamesadam@gmail.com>
2021-04-09 11:30:00 -0700

    Move server debugging env var file

---
+++ EpycDebugSample.sh
@@ -0,0 +1,15 @@
+# Copy this file to EpycDebug.sh and fill out
+
+# Mongo DB Connection string
+DB_CONNECTION_STRING=
+
+# Discord bot token
+DISCORD_BOT_TOKEN=
+
+# Leave this as-is
+NODE_ENV=development
+
+# Slack bot values
+SLACK_SOCKET_TOKEN=
+SLACK_CLIENT_ID=
+SLACK_CLIENT_SECRET=
