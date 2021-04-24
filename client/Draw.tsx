ac18b6836ab367cfc6ef453925053fea6f39bf97
Matt Adam <matthewjamesadam@gmail.com>
2021-04-24 10:30:34 -0700

    Show selected colour in custom colour list (#63)
    * Clean up tool and colour button usage
    
    * Show currently-selected colour in custom colour list

--- client/Draw.tsx
+++ client/Draw.tsx
@@ -1,123 +1,15 @@
 import * as React from 'react';
-import {
-    Alert,
-    Button,
-    ButtonGroup,
-    ButtonProps,
-    Card,
-    Overlay,
-    OverlayTrigger,
-    Popover,
-    ToggleButton,
-    ToggleButtonProps,
-} from 'react-bootstrap';
+import { Alert, Button, ButtonGroup, Card } from 'react-bootstrap';
 import { ToolType } from './DrawTypes';
 import { observer } from 'mobx-react-lite';
 import DrawManager from './DrawManager';
 import { HexColorPicker } from 'react-colorful';
 import { useAsyncAction } from './useAsyncAction';
 import { EpycApi } from './Apis';
-import Icon, { IconType } from './Icon';
+import { ToolButton, ToolToggleButton } from './ToolButton';
+import { ColourButton } from './ColourButton';
 
 const lineWidths = [1, 3, 5, 10];
-
-const rainbowStyle = {
-    background: 'linear-gradient(to right, orange , yellow, green, cyan, blue, violet)',
-    width: '5rem',
-    height: '5rem',
-};
-
-function PopupOverlay(props: React.PropsWithChildren<{ tooltip: string }>) {
-    return (
-        <OverlayTrigger
-            placement="top"
-            delay={{ show: 250, hide: 250 }}
-            overlay={
-                <Popover id="draw-popover">
-                    <Popover.Content>{props.tooltip}</Popover.Content>
-                </Popover>
-            }
-        >
-            {props.children}
-        </OverlayTrigger>
-    );
-}
-
-function ButtonIcon(props: { type: IconType }) {
-    return <Icon type={props.type} style={{ height: '1rem' }} fill="currentColor" display="block" />;
-}
-
-interface ToolButtonBaseProps {
-    tooltip: string;
-    icon?: IconType;
-}
-function ToolButtonBase<ButtonProps>(
-    Type: React.ComponentType<ButtonProps>,
-    props: React.PropsWithChildren<ButtonProps & ToolButtonBaseProps>
-) {
-    const content = props.icon ? <Icon type={props.icon} /> : props.children;
-
-    return (
-        <OverlayTrigger
-            placement="top"
-            delay={{ show: 250, hide: 400 }}
-            overlay={
-                <Popover id="draw-popover">
-                    <Popover.Content>{props.tooltip}</Popover.Content>
-                </Popover>
-            }
-        >
-            <Type {...props}>{content}</Type>
-        </OverlayTrigger>
-    );
-}
-
-const ToolButton = (props: React.PropsWithChildren<ButtonProps & ToolButtonBaseProps>) => ToolButtonBase(Button, props);
-
-const ToolToggleButton = (props: React.PropsWithChildren<ToggleButtonProps & ToolButtonBaseProps>) =>
-    ToolButtonBase(ToggleButton, props);
-
-function ColourButton(props: { colour: string | null | undefined; isSelected: boolean; onClick: () => void }) {
-    const style: React.CSSProperties = {
-        width: '1.3rem',
-        height: '1.3rem',
-        position: 'relative',
-    };
-
-    const buttonStyle: React.CSSProperties = {
-        padding: '0',
-        overflow: 'hidden',
-        backgroundColor: props.colour || 'white',
-        flexGrow: 0,
-    };
-
-    const hilightNode = (
-        <div
-            style={{
-                position: 'absolute',
-                top: -3,
-                bottom: -3,
-                right: -3,
-                left: -3,
-                border: '3px solid #000000',
-                borderRadius: '.2rem',
-                zIndex: 5,
-            }}
-        />
-    );
-
-    const hilight = props.isSelected ? hilightNode : null;
-
-    if (props.isSelected) {
-        buttonStyle.overflow = 'inherit';
-    }
-
-    return (
-        <Button key={props.colour} variant="outline-dark" style={buttonStyle} onClick={props.onClick}>
-            <div style={style}>{hilight}</div>
-        </Button>
-    );
-}
 
 function Draw(props: { gameName: string; frameId: string; title: string; onDone: () => void }) {
     const [drawManager] = React.useState<DrawManager>(() => new DrawManager(props.gameName, props.frameId));
@@ -161,108 +53,99 @@ function Draw(props: { gameName: string; frameId: string; title: string; onDone:
                     <ButtonGroup toggle size="sm" className="mr-1 mb-1">
                         {drawManager?.tools.map((tool) => {
                             return (
-                                <PopupOverlay key={tool.type} tooltip={tool.name}>
-                                    <ToggleButton
-                                        type="radio"
-                                        checked={tool.type === drawManager?.selectedTool.type}
-                                        variant="outline-dark"
-                                        onChange={() => {
-                                            drawManager?.setSelectedTool(tool.type);
-                                        }}
-                                    >
-                                        {tool.icon ? <ButtonIcon type={tool.icon} /> : tool.name}
-                                    </ToggleButton>
-                                </PopupOverlay>
+                                <ToolToggleButton
+                                    value={tool.type}
+                                    key={tool.type}
+                                    tooltip={tool.name}
+                                    type="radio"
+                                    icon={tool.icon}
+                                    checked={tool.type === drawManager?.selectedTool.type}
+                                    variant="outline-dark"
+                                    onChange={() => {
+                                        drawManager?.setSelectedTool(tool.type);
+                                    }}
+                                />
                             );
                         })}
                     </ButtonGroup>
 
                     <ButtonGroup toggle size="sm" className="mr-1 mb-1">
-                        <PopupOverlay tooltip="Select">
-                            <ToggleButton
-                                type="radio"
-                                checked={drawManager?.selectedTool.type === ToolType.select}
-                                variant="outline-dark"
-                                onChange={() => {
-                                    drawManager?.setSelectedTool(ToolType.select);
-                                }}
-                            >
-                                <ButtonIcon type="select" />
-                            </ToggleButton>
-                        </PopupOverlay>
+                        <ToolToggleButton
+                            value=""
+                            tooltip="Select"
+                            icon="select"
+                            type="radio"
+                            checked={drawManager?.selectedTool.type === ToolType.select}
+                            variant="outline-dark"
+                            onChange={() => {
+                                drawManager?.setSelectedTool(ToolType.select);
+                            }}
+                        />
 
-                        <PopupOverlay tooltip="Copy">
-                            <Button
-                                variant="outline-dark"
-                                disabled={drawManager?.copySource === null}
-                                onClick={() => {
-                                    drawManager?.copy();
-                                }}
-                            >
-                                <ButtonIcon type="copy" />
-                            </Button>
-                        </PopupOverlay>
+                        <ToolButton
+                            tooltip="Copy"
+                            icon="copy"
+                            variant="outline-dark"
+                            disabled={drawManager?.copySource === null}
+                            onClick={() => {
+                                drawManager?.copy();
+                            }}
+                        />
 
-                        <PopupOverlay tooltip="Paste">
-                            <ToggleButton
-                                type="radio"
-                                checked={drawManager?.selectedTool.type === ToolType.paste}
-                                variant="outline-dark"
-                                disabled={drawManager?.copyData === null}
-                                onChange={() => {
-                                    drawManager?.setSelectedTool(ToolType.paste);
-                                }}
-                            >
-                                <ButtonIcon type="paste" />
-                            </ToggleButton>
-                        </PopupOverlay>
+                        <ToolToggleButton
+                            value="paste"
+                            tooltip="Paste"
+                            type="radio"
+                            icon="paste"
+                            checked={drawManager?.selectedTool.type === ToolType.paste}
+                            variant="outline-dark"
+                            disabled={drawManager?.copyData === null}
+                            onChange={() => {
+                                drawManager?.setSelectedTool(ToolType.paste);
+                            }}
+                        />
                     </ButtonGroup>
 
                     <ButtonGroup size="sm" className="mr-1 mb-1">
-                        <PopupOverlay tooltip="Undo">
-                            <Button
-                                disabled={(drawManager?.ops.length || 0) <= 0}
-                                variant="outline-primary"
-                                onClick={() => {
-                                    drawManager?.undo();
-                                }}
-                            >
-                                <ButtonIcon type="undo" />
-                            </Button>
-                        </PopupOverlay>
+                        <ToolButton
+                            tooltip="Undo"
+                            icon="undo"
+                            disabled={(drawManager?.ops.length || 0) <= 0}
+                            variant="outline-primary"
+                            onClick={() => {
+                                drawManager?.undo();
+                            }}
+                        />
 
-                        <PopupOverlay tooltip="Redo">
-                            <Button
-                                disabled={(drawManager?.redos.length || 0) <= 0}
-                                variant="outline-primary"
-                                onClick={() => {
-                                    drawManager?.redo();
-                                }}
-                            >
-                                <ButtonIcon type="redo" />
-                            </Button>
-                        </PopupOverlay>
+                        <ToolButton
+                            tooltip="Redo"
+                            icon="redo"
+                            disabled={(drawManager?.redos.length || 0) <= 0}
+                            variant="outline-primary"
+                            onClick={() => {
+                                drawManager?.redo();
+                            }}
+                        />
                     </ButtonGroup>
 
                     <ButtonGroup toggle size="sm" className="mr-1 mb-1">
                         {lineWidths.map((lineWidth) => {
                             return (
-                                <PopupOverlay key={lineWidth} tooltip="Line Width">
-                                    <ToggleButton
-                                        type="radio"
-                                        variant="outline-dark"
-                                        checked={drawManager?.lineWidth === lineWidth}
-                                        onChange={() => {
-                                            if (drawManager) drawManager.lineWidth = lineWidth;
-                                        }}
-                                    >
-                                        <div className="d-flex align-items-center" style={{ height: '1rem' }}>
-                                            <div
-                                                style={{ width: '1.0rem', height: lineWidth, background: '#000000' }}
-                                            />
-                                        </div>
-                                    </ToggleButton>
-                                </PopupOverlay>
+                                <ToolToggleButton
+                                    value={lineWidth}
+                                    key={lineWidth}
+                                    tooltip="Line Width"
+                                    type="radio"
+                                    variant="outline-dark"
+                                    checked={drawManager?.lineWidth === lineWidth}
+                                    onChange={() => {
+                                        if (drawManager) drawManager.lineWidth = lineWidth;
+                                    }}
+                                >
+                                    <div className="d-flex align-items-center" style={{ height: '1rem' }}>
+                                        <div style={{ width: '1.0rem', height: lineWidth, background: '#000000' }} />
+                                    </div>
+                                </ToolToggleButton>
                             );
                         })}
                     </ButtonGroup>
---
+++ client/ToolButton.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import { Button, ButtonProps, OverlayTrigger, Popover, ToggleButton, ToggleButtonProps } from 'react-bootstrap';
+import { IconType } from './DrawTypes';
+import Icon from './Icon';
+
+/*
+    Styled buttons for tools in the drawing UI.
+    These buttons wrap the standard Button and ToggleButton with a tooltip, and
+    automatically add a content icon.
+*/
+
+interface ToolButtonBaseProps extends React.HTMLAttributes<HTMLElement> {
+    tooltip: string;
+    icon?: IconType;
+}
+
+function ToolButtonBase<Props>(
+    Type: React.ComponentType<Props>,
+    props: React.PropsWithChildren<Props & ToolButtonBaseProps>
+) {
+    const content = props.icon ? (
+        <Icon type={props.icon} style={{ height: '1rem' }} fill="currentColor" display="block" />
+    ) : (
+        props.children
+    );
+
+    return (
+        <OverlayTrigger
+            placement="top"
+            delay={{ show: 250, hide: 250 }}
+            overlay={
+                <Popover id="draw-popover">
+                    <Popover.Content>{props.tooltip}</Popover.Content>
+                </Popover>
+            }
+        >
+            <Type {...props}>{content}</Type>
+        </OverlayTrigger>
+    );
+}
+
+const ToolButton = (props: React.PropsWithChildren<ButtonProps & ToolButtonBaseProps>) => ToolButtonBase(Button, props);
+const ToolToggleButton = (props: React.PropsWithChildren<ToggleButtonProps & ToolButtonBaseProps>) =>
+    ToolButtonBase(ToggleButton, props);
+
+export { ToolButton, ToolToggleButton };
