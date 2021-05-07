ac8e47a0609e0da30214c95e1d0d80ad273d26b3
Matt <matthewjamesadam@gmail.com>
2021-05-07 11:48:42 -0700

    Await floating promises

--- server/Logger.ts
+++ server/Logger.ts
@@ -1,23 +1,31 @@
+import spacetime from 'spacetime';
+
 /*
     Centralized logging.  For now this just goes to the console.
 */
 export class Logger {
     static log(message: string) {
-        console.log(message);
+        console.log(`${this.timestamp} -- ${message}`);
     }
 
     static error(message: string) {
-        console.error(message);
+        console.error(`${this.timestamp} -- ${message}`);
     }
 
     static exception(error: any, message?: string) {
         if (message) {
-            console.error(message);
+            console.error(`${this.timestamp} -- ${message}`);
+        } else {
+            console.error(this.timestamp);
         }
 
         console.error(error);
         if (error.stack) {
             console.error(error.stack);
         }
+    }
+
+    static get timestamp(): string | object {
+        return spacetime.now().goto('America/Vancouver').format('nice');
     }
 }
