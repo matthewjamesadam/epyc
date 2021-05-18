f5b23d5415eb3d7c2fba52a0a9ab451a14c49eee
Matt <matthewjamesadam@gmail.com>
2021-05-18 12:50:19 -0700

    More updates

--- client/AllGameList.tsx
+++ client/AllGameList.tsx
@@ -1,37 +1,53 @@
 import * as React from 'react';
 import GameList from './GameList';
-import { useAsyncActionOnce } from './useAsyncAction';
+import { useAsyncActionNow } from './useAsyncAction';
 import { EpycApi } from './Apis';
 import { Container, Row, Col, Nav, Spinner } from 'react-bootstrap';
 
+type AllGameTypes = 'random' | 'recent';
+
 export default function AllGameList() {
-    const [isFetchingGames, games, gamesErr] = useAsyncActionOnce(() => EpycApi.getGames({ sampleSize: 5 }));
+    const [type, setType] = React.useState<AllGameTypes>('random');
+    const [isFetchingGames, games, gamesErr] = useAsyncActionNow(() => {
+        const query = type === 'random' ? { sampleSize: 25 } : { limit: 10 };
+        return EpycApi.getGames(query);
+    }, [type]);
 
     // WRONG
+
+    let content = (
+        <Row>
+            <Col>
+                <GameList games={games} />
+            </Col>
+        </Row>
+    );
+
     if (isFetchingGames) {
-        return <Spinner />;
+        content = <Spinner />;
     }
 
     return (
         <Container>
-            <Row className="mb-3">
+            {/* Re-add when we support time-based ordering */}
+            {/* <Row className="mb-3">
                 <Col>
-                    <Nav variant="pills">
+                    <Nav variant="pills" activeKey={type} onSelect={(type: AllGameTypes) => setType(type)}>
                         <Nav.Item>
-                            <Nav.Link active={true}>Blah</Nav.Link>
+                            <Nav.Link className="nav-link-small" eventKey="random">
+                                <small>Random</small>
+                            </Nav.Link>
                         </Nav.Item>
                         <Nav.Item>
-                            <Nav.Link>Blahzz</Nav.Link>
+                            <Nav.Link className="nav-link-small" eventKey="recent">
+                                <small>Recent</small>
+                            </Nav.Link>
                         </Nav.Item>
                     </Nav>
                 </Col>
-            </Row>
+            </Row> */}
 
-            <Row>
-                <Col>
-                    <GameList games={games} />
-                </Col>
-            </Row>
+            {content}
         </Container>
     );
 }
